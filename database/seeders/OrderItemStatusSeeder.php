46f1fe369eb447fed0a17bb620f1d576cf3ca5e0
brentongalien981 <brentongalien981@gmail.com>
2021-06-19 17:02:26 -0400

    for UCG-01: app prepares purchases, did TASK: - migrate - seed
    for UCG-01: app prepares purchases, did TASK:
    - migrate
    - seed

--- database/seeders/OrderItemStatusSeeder.php
+++ database/seeders/OrderItemStatusSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OrderItemStatusSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class OrderItemStatusSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('order_item_status')->insert(['code' => -304, 'name' => 'PURCHASE_INCOMPLETELY_RECEIVED']);
+        DB::table('order_item_status')->insert(['code' => -301, 'name' => 'EVALUATED_INCOMPLETELY_FOR_PURCHASE']);
+
+        DB::table('order_item_status')->insert(['code' => 300, 'name' => 'DEFAULT']);
+        DB::table('order_item_status')->insert(['code' => 301, 'name' => 'TO_BE_PURCHASED']);
+        DB::table('order_item_status')->insert(['code' => 302, 'name' => 'PURCHASED']);
+        DB::table('order_item_status')->insert(['code' => 303, 'name' => 'TO_BE_PURCHASE_RECEIVED']);
+        DB::table('order_item_status')->insert(['code' => 304, 'name' => 'PURCHASE_RECEIVED']);
+        DB::table('order_item_status')->insert(['code' => 305, 'name' => 'IN_STOCK']);
+
+        DB::table('order_item_status')->insert(['code' => 306, 'name' => 'TO_BE_PACKAGED']);
+        DB::table('order_item_status')->insert(['code' => 307, 'name' => 'PACKAGED']);
+
+        DB::table('order_item_status')->insert(['code' => 308, 'name' => 'TO_BE_DISPATCHED']);
+        DB::table('order_item_status')->insert(['code' => 309, 'name' => 'DISPATCHED']);
     }
 }
--- database/seeders/PurchaseStatusSeeder.php
+++ database/seeders/PurchaseStatusSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseStatusSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class PurchaseStatusSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('purchase_status')->insert(['code' => -304, 'name' => 'PURCHASE_INCOMPLETELY_RECEIVED']);
+        DB::table('purchase_status')->insert(['code' => -301, 'name' => 'EVALUATED_INCOMPLETELY_FOR_PURCHASE']);
+
+        DB::table('purchase_status')->insert(['code' => 300, 'name' => 'DEFAULT']);
+        DB::table('purchase_status')->insert(['code' => 301, 'name' => 'TO_BE_PURCHASED']);
+        DB::table('purchase_status')->insert(['code' => 302, 'name' => 'PURCHASED']);
+        DB::table('purchase_status')->insert(['code' => 303, 'name' => 'TO_BE_PURCHASE_RECEIVED']);
+        DB::table('purchase_status')->insert(['code' => 304, 'name' => 'PURCHASE_RECEIVED']);
+        DB::table('purchase_status')->insert(['code' => 305, 'name' => 'IN_STOCK']);
+
+        DB::table('purchase_status')->insert(['code' => 306, 'name' => 'TO_BE_PACKAGED']);
+        DB::table('purchase_status')->insert(['code' => 307, 'name' => 'PACKAGED']);
+
+        DB::table('purchase_status')->insert(['code' => 308, 'name' => 'TO_BE_DISPATCHED']);
+        DB::table('purchase_status')->insert(['code' => 309, 'name' => 'DISPATCHED']);
     }
 }
