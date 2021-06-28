660cce5b1a2084bbdfa42622adf0a391e5bafd08
brentongalien981 <brentongalien981@gmail.com>
2021-06-27 23:10:02 -0400

    finished UC-01.4: order-manager views automated-job-logs
    finished UC-01.4: order-manager views automated-job-logs

--- app/Http/Controllers/ScheduledTaskLogController.php
+++ app/Http/Controllers/ScheduledTaskLogController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ScheduledTaskLog;
 use Illuminate\Http\Request;
+use App\Models\ScheduledTaskLog;
+use App\Models\ScheduledTaskStatus;
 
 class ScheduledTaskLogController extends Controller
 {
     public function read(Request $r) {
 
+        $logs = ScheduledTaskLog::where('scheduled_task_id', $r->jobId)->orderBy('created_at', 'desc')->take(20)->get();
+
+        foreach ($logs as $l) {
+            $l->status_readable_name = ScheduledTaskStatus::where('code', $l->status_code)->get()[0]->readable_name;
+        }
+
         return [
             'isResultOk' => true,
             'objs' => [
-                'logs' => ScheduledTaskLog::where('scheduled_task_id', $r->jobId)->take(10)->get()
+                'logs' => $logs
             ]
         ];
     }
