639f1ad8a5b147477e5f8d3c1679ceb655c7e9e0
brentongalien981 <brentongalien981@gmail.com>
2021-07-04 01:50:37 -0400

    finished PP-01.1: load orders-stats section
    finished PP-01.1: load orders-stats section

--- app/Http/Controllers/DailySummaryController.php
+++ app/Http/Controllers/DailySummaryController.php
@@ -2,14 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\IncompleteOrder;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Exception;
 use Illuminate\Http\Request;
 
 class DailySummaryController extends Controller
 {
     public function readDailySummaryData(Request $r)
     {
+        $endDate = $r->statsEndDate . ' 23:59:59';
+
+        $numOfOrders = Order::where('created_at', '>=', $r->statsStartDate)
+            ->where('created_at', '<=', $endDate)
+            ->count();
+
+
+        $numOfOrderItems = OrderItem::where('created_at', '>=', $r->statsStartDate)
+            ->where('created_at', '<=', $endDate)
+            ->count();
+
+
+        $numOfIncompleteOrders = IncompleteOrder::where('created_at', '>=', $r->statsStartDate)
+            ->where('created_at', '<=', $endDate)
+            ->count();
+
+
+
         return [
-            'msg' => ' In METHOD: readDailySummaryData()'
+            'isResultOk' => true,
+            'objs' => [
+                'numOfOrders' => $numOfOrders,
+                'numOfOrderItems' => $numOfOrderItems,
+                'numOfIncompleteOrders' => $numOfIncompleteOrders
+            ]
         ];
     }
 }
