46bdb28d0f8e0e24a38ef913c3ad662614e4f5a4
odox700 <odox700@gmail.com>
2021-09-20 14:22:16 -0400

    for UCG-03: dispatch-manager evaluates and flags order to be packaged, did TASKS: ✔ seed additional order-statuses ✔ seed additional order-item-statuses  ✔ add relevant columns to TABLE: inventory_items
    for UCG-03: dispatch-manager evaluates and flags order to be packaged, did TASKS:
    ✔ seed additional order-statuses
    ✔ seed additional order-item-statuses
    ✔ add relevant columns to TABLE: inventory_items

--- database/seeders/OneTimeSeeder.php
+++ database/seeders/OneTimeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OneTimeSeeder extends Seeder
 {
@@ -14,8 +15,12 @@ class OneTimeSeeder extends Seeder
     public function run()
     {
 
-        $this->call([
-            //
-        ]);
+        DB::table('order_item_status')->insert(['code' => 310, 'name' => 'BEING_PACKAGED']);
+
+        DB::table('order_item_status')->insert(['code' => -310, 'name' => 'MISSING_ORDER_ITEM']);
+        DB::table('order_item_status')->insert(['code' => -311, 'name' => 'BROKEN_ORDER_ITEM']);
+        DB::table('order_item_status')->insert(['code' => -312, 'name' => 'TOO_LATE_TO_DELIVER']);
+        DB::table('order_item_status')->insert(['code' => -313, 'name' => 'TOO_EXPENSIVE_TO_DELIVER']);
+        DB::table('order_item_status')->insert(['code' => -314, 'name' => 'OTHER_ORDER_ITEM_PROBLEMS']);
     }
 }
--- database/seeders/OrderItemStatusSeeder.php
+++ database/seeders/OrderItemStatusSeeder.php
@@ -17,6 +17,16 @@ class OrderItemStatusSeeder extends Seeder
         DB::table('order_item_status')->insert(['code' => -304, 'name' => 'PURCHASE_INCOMPLETELY_RECEIVED']);
         DB::table('order_item_status')->insert(['code' => -301, 'name' => 'EVALUATED_INCOMPLETELY_FOR_PURCHASE']);
 
+        DB::table('order_item_status')->insert(['code' => -310, 'name' => 'MISSING_ORDER_ITEM']);
+        DB::table('order_item_status')->insert(['code' => -311, 'name' => 'BROKEN_ORDER_ITEM']);
+        DB::table('order_item_status')->insert(['code' => -312, 'name' => 'TOO_LATE_TO_DELIVER']);
+        DB::table('order_item_status')->insert(['code' => -313, 'name' => 'TOO_EXPENSIVE_TO_DELIVER']);
+        DB::table('order_item_status')->insert(['code' => -314, 'name' => 'OTHER_ORDER_ITEM_PROBLEMS']);
+
+        
+
+
+        
         DB::table('order_item_status')->insert(['code' => 300, 'name' => 'DEFAULT']);
         DB::table('order_item_status')->insert(['code' => 301, 'name' => 'TO_BE_PURCHASED']);
         DB::table('order_item_status')->insert(['code' => 302, 'name' => 'PURCHASED']);
@@ -25,9 +35,10 @@ class OrderItemStatusSeeder extends Seeder
         DB::table('order_item_status')->insert(['code' => 305, 'name' => 'IN_STOCK']);
 
         DB::table('order_item_status')->insert(['code' => 306, 'name' => 'TO_BE_PACKAGED']);
+        DB::table('order_item_status')->insert(['code' => 310, 'name' => 'BEING_PACKAGED']);
         DB::table('order_item_status')->insert(['code' => 307, 'name' => 'PACKAGED']);
 
         DB::table('order_item_status')->insert(['code' => 308, 'name' => 'TO_BE_DISPATCHED']);
-        DB::table('order_item_status')->insert(['code' => 309, 'name' => 'DISPATCHED']);
+        DB::table('order_item_status')->insert(['code' => 309, 'name' => 'DISPATCHED']);                
     }
 }
